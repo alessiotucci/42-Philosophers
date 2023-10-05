e980328fd38ae6e07bf060f3b3d54bf448e3df6c
Alessio Tucci <alessiotucci33@gmail.com>
2023-10-05 11:25:27 +0200

    created the makefile and a little progress in the header

---
+++ philo/philo.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 13:26:42 by atucci            #+#    #+#             */
+/*   Updated: 2023/10/05 11:24:58 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <stdint.h>
+# include <stdbool.h>
+
+# define WRONG_INPUT 1
+# define MALLOC_ERROR 2
+
+# define TAKE_FORKS "has taken a fork"
+# define THINK "is thinking"
+# define SLEEP "is sleeping"
+# define EAT "is eating"
+# define DIED "died"
+
+/* Define color macros */
+# define GREEN   "\033[1;32m"
+# define CYAN    "\033[1;36m"
+# define YELLOW  "\033[1;33m"
+# define RED     "\033[1;31m"
+# define RESET   "\033[0m"
+/* Define Background colors macros */
+# define BG_RESET        "\033[49m"
+# define BG_GREEN        "\033[42m"
+# define BG_CYAN         "\033[46m"
+# define BG_YELLOW       "\033[43m"
+# define BG_RED          "\033[41m"
+#endif
