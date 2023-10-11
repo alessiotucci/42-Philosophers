311ce78cecc25302a79e746f8e801cd30778a7b0
Alessio Tucci <alessiotucci33@gmail.com>
2023-10-11 12:58:25 +0200

    action of routine declared and started to be implemented

---
+++ philo/actions_of_routine.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions_of_routine.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 12:52:01 by atucci            #+#    #+#             */
+/*   Updated: 2023/10/11 12:57:53 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/* To start eating we need two forks*/
+void	eat(t_plato *philo)
+{
+
+}
+/* We need to think for a certaing amount of time */
+void	think(t_plato *philo)
+{
+
+}
+/*We need to sleep for a certain amount of time*/
+void	sleep(t_plato *philo)
+{
+
+}
