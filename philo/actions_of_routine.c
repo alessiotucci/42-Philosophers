be4f9d9fe6e0f72d804d22125a66bed3a9fdc9c6
Alessio Tucci <alessiotucci33@gmail.com>
2023-10-12 15:16:02 +0200

    the taking fork action is putteed inside a single function: FIXED case with 2 philos

--- philo/actions_of_routine.c
+++ philo/actions_of_routine.c
@@ -6,19 +6,27 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 12:52:01 by atucci            #+#    #+#             */
-/*   Updated: 2023/10/12 15:07:22 by atucci           ###   ########.fr       */
+/*   Updated: 2023/10/12 15:14:17 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void take_forks(t_plato *philo)
+{
+	pthread_mutex_lock(philo->right_fork);
+	console_write(philo->table, philo->name, TAKE_FORK);
+	pthread_mutex_lock(philo->left_fork);
+	console_write(philo->table, philo->name, TAKE_FORK);
+}
 /* To start eating we need two forks*/
 void	eats(t_plato *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
+	/*pthread_mutex_lock(philo->left_fork);
 	console_write(philo->table, philo->name, TAKE_FORK);
 	pthread_mutex_lock(philo->right_fork);
-	console_write(philo->table, philo->name, TAKE_FORK);
+	console_write(philo->table, philo->name, TAKE_FORK);*/
+	take_forks(philo);
 	// perform the action of eating for a certain amount of time
 	console_write(philo->table, philo->name, EAT);
 	//printf("%llu %d is eating\n", (my_get_time() - philo->table->time_of_start), philo->name);
