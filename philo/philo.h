9d2438c2c5a952333fa27fc5de40e9c9344a1649
Alessio Tucci <alessiotucci33@gmail.com>
2023-10-25 10:37:05 +0200

    commmented nicely the header

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:26:42 by atucci            #+#    #+#             */
-/*   Updated: 2023/10/24 14:33:04 by atucci           ###   ########.fr       */
+/*   Updated: 2023/10/25 10:36:45 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,8 @@
 # define BG_YELLOW       "\033[43m"
 # define BG_RED          "\033[41m"
 
-/* this for testing */
+/* this for testing, useful to
+	* parse the input correctly*/
 typedef struct s_input
 {
 	int	how_many;
@@ -56,69 +57,82 @@ typedef struct s_input
 	int	time_to_sleep;
 	int	often_eat;
 }		t_input;
-/**/
+
+/* this is the struct for a philospher
+	* I call it plato tho*/
 typedef struct s_plato
 {
 	struct s_table		*table;
 	pthread_t		thread;
 	int		name;
-
+// this mutex is lock the state: alive or is_eating
 	pthread_mutex_t	state_of_philo;
 	int		alive;
 	int		is_eating;
-
+// this mutex is to lock the number of meal eaten by philos
 	pthread_mutex_t	meals_lock;
 	int		meal_eaten;
-
+// this mutex is to lock the last time eat
 	pthread_mutex_t	eat_last_time;
 	size_t		last_time_eat;
-
+// those are the value to pass to my_usleep();
 	size_t		time_to_die;
 	size_t		time_to_eat;
 	size_t		time_to_sleep;
-	// those are mutexes
+	// those are the forks
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 }		t_plato;
-/* the table where the philosopher are sitting*/
 
+/* the table where the philosopher are sitting
+	* they are sharing the forks of course*/
 typedef struct s_table
 {
 	t_plato		*philly;
 	int		philly_size;
+
 	pthread_mutex_t	*few_forks;
-	pthread_mutex_t	lock_table;
-	pthread_mutex_t	writing;
-	//this is the monitor thread and needs some var
-	//to keep checking the philospher sitted at the table
-	int		someone_is_dead;
+
+	pthread_mutex_t	lock_table; // locking the table if someone is dying
+
+	pthread_mutex_t	writing; // locking to write with the console write
+
+	int		someone_is_dead; 
 	int		enough_is_enough;
+	int		meals_to_eat;
+	//this is the monitor thread and needs some var
 	pthread_t		monitor;
+
 	size_t		time_to_die;
 	size_t		time_to_eat;
 	size_t		time_to_sleep;
+
 	u_int64_t		time_of_start;
-	int		meals_to_eat;
 }		t_table;
 
 /* utils function */
 int	ft_isdigit(int argum);
 long	ft_atoi_plus(const char *str);
+
 /* modified version of time.h function*/
 int	my_usleep(u_int64_t interlude);
 u_int64_t	my_get_time(void);
+
 /* lay the tables means gathering data off all the philos and forks*/
 int	lay_the_table(t_input *params, t_table *new_table);
 int	create_name_philos(t_table *new_table);
+
 /* this function is for printing debugg*/
 void print_struct(t_input *commmand, int flag);
 void	print_table(t_table *new_table);
 void	parsing_argus(t_input *params, char *av[]);
 void	start_routine(t_table *the_table, int how_many);
 void	*routing(void *argum);
+
 /* let try to be leaks free*/
 void	free_all(t_table *old_table);
 void	*monitoring(void *argum);
+
 /* Action of the routine*/
 void	eats(t_plato *philo);
 void	thinks(t_plato *philo);
