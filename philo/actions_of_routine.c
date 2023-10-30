44c6909b65639698869deb18d04389d51d741f4c
atucci <alessiotucci33@gmail.com>
2023-10-30 12:49:57 +0100

    actions of routing norminetteeed except the comments

--- philo/actions_of_routine.c
+++ philo/actions_of_routine.c
@@ -6,7 +6,7 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 12:52:01 by atucci            #+#    #+#             */
-/*   Updated: 2023/10/30 12:03:24 by atucci           ###   ########.fr       */
+/*   Updated: 2023/10/30 12:49:21 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ static int	take_forks(t_plato *philo, int flag)
 		my_usleep(philo->time_to_die);
 		return (1);
 	}
-		//printf("%s TAKE FORK!\n %llu  check the table result: %d\tphilo->alive[%d]%s\n", RED, my_get_time() - philo->table->time_of_start, check_table(philo->table), philo->alive, RESET);
-	if (check_table(philo->table) == 0 && philo->alive == 1 && philo->philo_is_full == 0)
+		//printf("%s TAKE FORK!\n %llu 
+		//check the table result: %d\tphilo->alive[%d]%s\n", RED,
+		//my_get_time() - philo->table->time_of_start,
+		//check_table(philo->table), philo->alive, RESET);
+	if (check_table(philo->table) == 0
+		&& philo->alive == 1 && philo->philo_is_full == 0)
 	{
 		if (check_table(philo->table) || philo->alive == 0)
 			return (1);
@@ -39,14 +43,15 @@ static int	take_forks(t_plato *philo, int flag)
 }
 
 /*drop forks means unlock the mutexes*/
-static int drops_forks(t_plato *philo)
+static int	drops_forks(t_plato *philo)
 {
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_lock(&philo->state_of_philo);
 	philo->is_eating = 0;
 	pthread_mutex_unlock(&philo->state_of_philo);
-	//printf("%sdrop forks %d-> check table is [%d]%s\n", BG_RED, philo->name, check_table(philo->table), BG_RESET);
+	//printf("%sdrop forks %d-> check table is [%d]%s\n",
+	//BG_RED, philo->name, check_table(philo->table), BG_RESET);
 	if (check_table(philo->table) == 1)
 		return (1);
 	else
@@ -63,8 +68,12 @@ int	eats(t_plato *philo)
 		return ((take_forks(philo, 1)));
 	else
 	{
-//		printf("%s LET EAT(%d)\n %llu check the table result: %d\tphilo->alive[%d]%s\n", PURPLE, philo->name, my_get_time() - philo->table->time_of_start, check_table(philo->table), philo->alive, RESET);
-		if (!check_table(philo->table) && philo->alive == 1 && philo->philo_is_full == 0)
+//		printf("%s LET EAT(%d)\n %llu check the table result:
+//		%d\tphilo->alive[%d]%s\n", PURPLE, philo->name,
+//		my_get_time() - philo->table->time_of_start,
+//		check_table(philo->table), philo->alive, RESET);
+		if (!check_table(philo->table)
+			&& philo->alive == 1 && philo->philo_is_full == 0)
 		{
 			if (take_forks(philo, 0))
 				return (1);
@@ -75,7 +84,9 @@ int	eats(t_plato *philo)
 			pthread_mutex_unlock(&philo->state_of_philo);
 			pthread_mutex_lock(&philo->eat_last_time);
 			philo->last_time_eat = my_get_time() + philo->time_to_die;
-	//printf("%supdate[%d]%s\nlast time eat is(%llu+%zu)= %zu\n", BG_RED, philo->name, BG_RESET, my_get_time(), philo->time_to_die, philo->last_time_eat);
+	//printf("%supdate[%d]%s\nlast time eat is(%llu+%zu)= %zu\n",
+	//BG_RED, philo->name, BG_RESET, my_get_time(),
+	//philo->time_to_die, philo->last_time_eat);
 			pthread_mutex_unlock(&philo->eat_last_time);
 			pthread_mutex_lock(&philo->meals_lock);
 			philo->meal_eaten++;
@@ -90,7 +101,8 @@ int	eats(t_plato *philo)
 /* We need to think for a certaing amount of time*/
 int	thinks(t_plato *philo)
 {
-	if (check_table(philo->table) == 0)// && philo->philo_is_full == 0) // condition needed
+	if (check_table(philo->table) == 0)
+		// && philo->philo_is_full == 0) // condition needed
 	{
 		console_write(philo->table, philo->name, THINK, GRAY);
 		return (0);
@@ -101,7 +113,8 @@ int	thinks(t_plato *philo)
 /*We need to sleep for a certain amount of time*/
 int	sleeps(t_plato *philo)
 {
-	if (check_table(philo->table) == 0)// && philo->philo_is_full == 0) // condition needed
+	if (check_table(philo->table) == 0)
+		// && philo->philo_is_full == 0) // condition needed
 	{
 		console_write(philo->table, philo->name, SLEEP, CYAN);
 		my_usleep(philo->time_to_sleep);
